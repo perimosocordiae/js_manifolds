36e00947f9e7ac117ef08d360c8f4d874a151499
CJ Carey <perimosocordiae@gmail.com>
2016-08-30 15:26:35 -0400

    Converting lapeig code

---
+++ laplacian_eigenmaps.js
@@ -0,0 +1,54 @@
+(function() {
+  var root = typeof exports !== "undefined" && exports !== null ? exports : this;
+
+  if (typeof require !== "undefined" && require !== null) {
+    root.numeric = require('./vendor/numeric-1.2.6.min.js');
+    root.mca = require('./power_method').mca;
+  }
+
+  function _comb_graph_laplacian(dists) {
+    var L = numeric.mul(dists, -1);
+    var n = dist.length;
+    for (var i = 0; i < n; i++) {
+      L[i][i] = numeric.sum(dists[i]) + 1e-10;
+    }
+    return L;
+  };
+
+  function _normed_graph_laplacian(dists) {
+    var n = dists.length;
+    var d = [];
+    for (i = 0; i < n; i++) {
+      d.push(Math.sqrt(numeric.sum(dists[i])))
+    }
+    var L = numeric.clone(dists);
+    for (i = 0; i < n; i++) {
+      for (j = 0; j < n; j++) {
+        L[i][j] /= -d[i] * d[j];
+      }
+      L[i][i] = 1;
+    }
+    return L;
+  };
+
+  this.lapeig = function(dists, num_dims, axis_scale) {
+    if (axis_scale == null) {
+      axis_scale = 50;
+    }
+    var L = _normed_graph_laplacian(dists);
+    var tmp = root.mca(L, num_dims + 1, 100, 1e-12, 3),
+        lambda = tmp[0],
+        E = tmp[1];
+    var start_idx = 0;
+    for (var i = 0; i <= num_dims; i++) {
+      if (lambda[i] < 1e-6) {
+        start_idx++;
+        continue;
+      }
+      numeric.muleq(E[i], axis_scale / numeric.norminf(E[i]));
+    }
+    E.splice(0, start_idx);
+    return numeric.transpose(E);
+  };
+
+}).call(this);
